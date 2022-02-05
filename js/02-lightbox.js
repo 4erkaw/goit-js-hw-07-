58ed8afa198969b92d2432c56679105384bfc3c7
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-02-05 19:44:10 +0200

    task 2 incompleted

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => (
+    `<li><a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a></li>`
+    )
+  )
+  .join(" ");
+
+gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+gallery.addEventListener("click", onGalleryElemClick);
+
+let lightbox = null
+
+function onGalleryElemClick(e) {
+    e.preventDefault();
+    const origImg = e.target.dataset.source;
+lightbox = SimpleLightbox("a")
+}
